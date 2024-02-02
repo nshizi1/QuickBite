383d2c71a7dd336b528192f070b18be8df8e994d
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2024-02-01 20:24:54 -0800

    messages

--- frontend/src/components/Coming.jsx
+++ frontend/src/components/Coming.jsx
@@ -1,13 +1,44 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Modal } from 'react-responsive-modal';
 import 'react-responsive-modal/styles.css';
+import { ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Coming() {
     const  year = new Date().getFullYear();
     const [open, setOpen] = useState(false);
 
-  const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setOpen(false);
+    const onOpenModal = () => setOpen(true);
+    const onCloseModal = () => setOpen(false);
+
+    const [email, setEmail] = useState('');
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
+    const form = useRef();
+    const [loading, setLoading] = useState(false);
+    const notify = (event) => {
+        event.preventDefault();
+        if(email.trim()===""){
+            toast.error('Please enter your email');
+        }else{
+            toast.success("Email sent successfully");
+            setLoading(false)
+            // setLoading(true)
+            // emailjs.sendForm('service_p43m6qi', 'template_r7qrk2e', form.current, 'IOhB6Nxg-_2KrNZWL')
+            // the above template key and service key are not the ones to be used, they will be changed later
+            // .then((result) => {
+            //     toast.success("Email sent successfully");
+            //     setEmail("");
+            // }, (error) => {
+            // toast.error("failed, try again later");
+            //     setEmail("");
+            // })
+            // .finally(() => {
+            //     setLoading(false);
+            // });
+        }
+    }
   return (
     <section id="coming" className="flex flex-col items-center justify-between px-20 py-10">
         <header className="flex items-center justify-between w-full text-blue-400">
@@ -27,13 +58,14 @@ function Coming() {
             <p className="text-xl">It will be here soon, click &quot;Notify me&quot; button to be ready for the grand day!</p>
             <button onClick={onOpenModal} className="px-6 py-2 font-bold transition ease-in-out bg-blue-500 rounded-md hover:bg-blue-600 ">Notify me</button>
             <Modal open={open} onClose={onCloseModal} center>
-                <form className="flex flex-col items-start gap-2 ">
+                <form ref={form} className="flex flex-col items-start gap-2 ">
                     <p>Enter your email:</p>
-                    <input type="email" className="h-8 px-3 rounded outline-none w-96 max-sm:w-60"/>
-                    <button type="submit" className="px-4 py-1 text-white transition ease-in-out bg-blue-900 rounded-md hover:bg-blue-950">Confirm</button>
+                    <input type="email" value={email} onChange={handleEmailChange} className="h-8 px-3 rounded outline-none w-96 max-sm:w-60"/>
+                    <button type="button" onClick={notify} disabled={loading} className="px-4 py-1 text-white transition ease-in-out bg-blue-900 rounded-md hover:bg-blue-950">{loading ? 'Loading...' : 'Confirm'}</button>
                 </form>
+                
             </Modal>
-            
+            <ToastContainer />
         </div>
         <footer className="flex flex-col items-center text-xl text-white">
             <p>{year} &copy; <a href="#" className="font-bold">Trave designs</a>. All rights reserved.</p>
@@ -45,6 +77,7 @@ function Coming() {
                 <li><a href="#"><i className="text-2xl text-purple-600 fa-solid fa-globe"></i></a></li>
             </ul>
         </footer>
+        
     </section>
   )
 }
